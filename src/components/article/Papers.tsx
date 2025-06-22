5076855adf540a79fb3b86c462b3eff4e0e18e7b
zehata <zehata@gmail.com>
2025-06-22 13:39:13 +0800

    Fixed mismatched slant of content with paper. Fixed overlapping article stamps on mobile layout.

--- src/components/article/Papers.tsx
+++ src/components/article/Papers.tsx
@@ -1,8 +1,8 @@
 const Papers = () => {
   return (
     <>
-      <div className="absolute bg-background w-[103%] h-[120%] -rotate-4 shadow-[0_0_10px_-5px]"></div>
-      <div className="absolute bg-background w-[103%] h-[120%] -rotate-1 shadow-[0_0_10px_-5px]"></div>
+      <div className="absolute bg-background w-[100%] h-[120%] -rotate-4 shadow-[0_0_10px_-5px]"></div>
+      <div className="absolute bg-background w-[100%] h-[120%] -rotate-1 origin-[50%_1%] shadow-[0_0_10px_-5px]"></div>
     </>
   );
 };
