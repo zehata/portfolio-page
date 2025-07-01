b2251d01ceae3856f700bb4ab52f11082c45472e
zehata <zehata@gmail.com>
2025-07-01 13:20:40 +0800

    Linted

--- src/components/about/SpeechBubble.tsx
+++ src/components/about/SpeechBubble.tsx
@@ -92,39 +92,35 @@ const SpeechBubble = ({
           {dialog}
         </div>
         <div className="relative h-10 flex-shrink-0">
-        <div
-          className={classNames(
-            "absolute w-12 h-10 overflow-hidden",
-            {
+          <div
+            className={classNames("absolute w-12 h-10 overflow-hidden", {
               ["right-0"]: shouldExpand && !isProceeding,
               ["right-5"]: !shouldExpand,
               ["-right-5 opacity-0"]: isProceeding,
-            },
-          )}
-          style={{
-            transition: shouldExpand
-              ? isProceeding
-                ? "250ms ease-out"
-                : "250ms ease-out 500ms"
-              : "none",
-          }}
-          onClick={skip}
-        >
-          <div
-            className={classNames(
-              "absolute top-[calc(1.25rem-2px)] right-2 h-1 bg-foreground",
-              {
-                ["w-0"]: shouldExpand,
-                ["w-10"]: !shouldExpand,
-              },
-            )}
+            })}
             style={{
-              transition: shouldExpand ? "1000ms ease-out 1000ms" : "none",
+              transition: shouldExpand
+                ? isProceeding
+                  ? "250ms ease-out"
+                  : "250ms ease-out 500ms"
+                : "none",
             }}
-          ></div>
-          <div className="absolute right-3 border-t-4 border-r-4 w-10 h-10 rotate-45"></div>
-        </div>
-
+            onClick={skip}
+          >
+            <div
+              className={classNames(
+                "absolute top-[calc(1.25rem-2px)] right-2 h-1 bg-foreground",
+                {
+                  ["w-0"]: shouldExpand,
+                  ["w-10"]: !shouldExpand,
+                },
+              )}
+              style={{
+                transition: shouldExpand ? "1000ms ease-out 1000ms" : "none",
+              }}
+            ></div>
+            <div className="absolute right-3 border-t-4 border-r-4 w-10 h-10 rotate-45"></div>
+          </div>
         </div>
       </div>
     </>
