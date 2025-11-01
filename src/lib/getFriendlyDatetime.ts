a0fc3aa4ea057af87c00a0398c3c99646a52601f
zehata <zehata@gmail.com>
2025-11-01 22:11:18 +0800

    Refactored friendly date time logic

--- src/lib/getFriendlyDatetime.ts
+++ src/lib/getFriendlyDatetime.ts
@@ -1,17 +1,18 @@
 "use client";
 
-const hourDuration = 3600000 as const;
+export const HOUR_DURATION = 3600000 as const;
 
-export const getFriendlyDatetime = (date: number) => {
-  const timeAtStartOfDay = new Date().setHours(0, 0, 0, 0);
-  const timeSinceStartOfDay = timeAtStartOfDay - date;
-  if (Date.now() - date < 1 * hourDuration) return "(Just now)";
+export const getFriendlyDatetime = (timestamp: number, currentDate: Date) => {
+  if (currentDate.getTime() - timestamp < 1 * HOUR_DURATION)
+    return "(Just now)";
+  const timeAtStartOfDay = currentDate.setHours(0, 0, 0, 0);
+  const timeSinceStartOfDay = timeAtStartOfDay - timestamp;
   if (timeSinceStartOfDay < 0) return "(Today)";
-  if (timeSinceStartOfDay < 24 * hourDuration) return "(Yesterday)";
-  if (timeSinceStartOfDay < 2 * 24 * hourDuration)
+  if (timeSinceStartOfDay < 24 * HOUR_DURATION) return "(Yesterday)";
+  if (timeSinceStartOfDay < 2 * 24 * HOUR_DURATION)
     return "(A couple of days ago)";
-  if (timeSinceStartOfDay < 7 * 24 * hourDuration) return "(A few days ago)";
-  if (timeSinceStartOfDay < 14 * 24 * hourDuration) return "(Last week)";
+  if (timeSinceStartOfDay < 7 * 24 * HOUR_DURATION) return "(A few days ago)";
+  if (timeSinceStartOfDay < 14 * 24 * HOUR_DURATION) return "(Last week)";
   return "";
 };
 
