c3df266ae181014d27c718d429557a897ecfab75
zehata <zehata@gmail.com>
2025-11-01 22:10:32 +0800

    Fixed tests setup

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    transpilePackages: ["slonik", "p-limit", "yocto-queue"]
+};
 
 export default nextConfig;
