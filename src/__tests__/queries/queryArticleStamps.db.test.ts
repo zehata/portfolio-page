957d843f07e9eaef1d98e96b9d774031f32045ca
zehata <zehata@gmail.com>
2026-07-24 23:21:21 +0800

    replace database connection code

--- src/__tests__/queries/queryArticleStamps.db.test.ts
+++ src/__tests__/queries/queryArticleStamps.db.test.ts
@@ -1,20 +1,17 @@
-import {
-  requestConnectionPool,
-  requestConnectionPoolEnd,
-} from "@/lib/connection";
 import { ArticleType } from "@/lib/types";
 import queryArticleStamps from "@/queries/queryArticleStamps";
 import { QUERY_TEST_BLOG_ID } from "../testlibs/testUuids";
+import { createConnectionPool, endConnectionPool } from "@/lib/connection";
 
 describe(queryArticleStamps, () => {
   test("blogs query resulls should match snapshot", async () => {
-    const pool = await requestConnectionPool();
+    const pool = await createConnectionPool();
     const result = await queryArticleStamps(
       pool,
       ArticleType.Blog,
       QUERY_TEST_BLOG_ID,
     );
     expect(result).toMatchSnapshot();
-    await requestConnectionPoolEnd();
+    await endConnectionPool(pool);
   });
 });
--- src/lib/getArticle.ts
+++ src/lib/getArticle.ts
@@ -5,19 +5,19 @@ import { ArticleType, tables } from "@/lib/types";
 import { cacheLife, cacheTag } from "next/cache";
 import queryArticleStamps from "@/queries/queryArticleStamps";
 import { keyBy } from "lodash";
-import { requestConnectionPool, requestConnectionPoolEnd } from "./connection";
+import { createConnectionPool, endConnectionPool } from "./connection";
 
 export const getArticle = async (articleType: ArticleType, id: string) => {
   "use cache";
   cacheLife({ expire: 60 });
   cacheTag(tables[articleType]);
 
-  const pool = await requestConnectionPool();
+  const pool = await createConnectionPool();
 
   const data = await queryArticle(pool, articleType, id);
   const stampsData = await queryArticleStamps(pool, articleType, id);
 
-  requestConnectionPoolEnd();
+  await endConnectionPool(pool);
 
   return {
     id: data.id,
